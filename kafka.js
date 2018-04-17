33bab2ad10529eca952ad025998ed9bb100401fb
Skyler Brungardt <skyler.brungardt@gmail.com>
2018-04-16 21:05:19 -0700

    Observing and producing on collections

--- kafka.js
+++ kafka.js
@@ -1,11 +1,16 @@
 
 const harbor_name = 'kafka';
-const dependencies = ['debug', 'elytron'].join(' ');
+const dependencies = [
+  'debug',
+  'elytron',
+  'underscore',
+].join(' ');
 
 require('child_process').execSync(`npm i ${dependencies}`);
 
 const log = require('debug')(`${harbor_name}:log`);
 const error = require('debug')(`${harbor_name}:error`);
+const produce = require('elytron').produce;
 
 log(`Dependencies installed: ${dependencies}`);
 
@@ -19,12 +24,12 @@ collections.forEach((collection) => {
   collection_names.push(collection.name);
 });
 
-const render_input = (values) => {
+const render_input = (values = {}) => {
 
   const html = `
     <p>This harbor submits manifests to a Kafka broker.</p>
     <p>If configured as a Followup Lane to another lane, it will produce its 
-    manifest to the Topic configured here when called.</p>
+    manifest (and prior manifest) to the Topic configured here when called.</p>
     <p>If database collections are selected here, it will observe any changes 
     to those collections, and produce them to the Topic configured here.</p>
     <p>If the same Lane is configured to do both, it will produce events from
@@ -36,6 +41,7 @@ const render_input = (values) => {
         name=kafka-broker-connection-string
         placeholder="kafka1,kafka2,kafka3 (etc., comma separated)"
         required
+        value="${values['kafka-broker-connection-string'] || ''}"
       >
     </label>
     <label><h5>Topic:</h5>
@@ -44,6 +50,7 @@ const render_input = (values) => {
         name=kafka-topic
         placeholder="Example-Topic_Name.1 (alphanum, dash, underscore, dot)"
         required
+        value="${values['kafka-topic'] || ''}"
       >
     </label>
     <h5>Collections found:</h5>
@@ -53,10 +60,10 @@ const render_input = (values) => {
           <li>
             <label>
               <input
-                name="${name}"
+                name="collection:${name.replace('.', '_dot_')}"
                 type=checkbox
-                value="${name}"
-                ${values && values[name] ? 'checked' : ''}
+                value="${name.replace('.', '_dot_')}"
+                ${values && values[`collection:${name}`] ? 'checked' : ''}
               >
               ${name}
             </label>
@@ -73,7 +80,45 @@ const render_work_preview = () => `Nothing yet!`;
 
 const register = () => harbor_name;
 
-const update = () => true;
+const update = (lane, values) => {
+  if (! check_topic(values['kafka-topic'])) {
+    error(`Invalid values passed for harbormaster-kafka config: ${values}`);
+    return false;
+  }
+
+  set_hooks(values);
+  return true;
+};
+
+const check_topic = (topic_name) => {
+  const allowed_characters = /[a-z0-9_\.-]/gi;
+
+  const results = topic_name.match(allowed_characters);
+  return results.length == topic_name.length;
+};
+
+const set_hooks = (values) => {
+  const keys = Object.keys(values);
+  const collection_key = /^collection:/;
+  const topic = values['kafka-topic'];
+
+  _.each(hooks, hook => hook.stop());
+
+  keys.forEach((key) => {
+    if (!key.match(collection_key)) return;
+    const collection_name = values[key].replace('_dot_', '.');
+    const collection = Meteor.Collection.get(collection_name);
+    hooks[collection] = collection.find().observe({
+      added (doc) { return produce(topic, { type: 'added', doc }); },
+      removed (old) { return produce(topic, { type: 'removed', old }); },
+      changed (old, doc) {
+        return produce(topic, { type: 'changed', old, doc });
+      },
+    });
+  });
+};
+
+const hooks = {};
 
 const work = () => {};
 
